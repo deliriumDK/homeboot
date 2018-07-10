fe58754d3c423251cc40d3f68759dc088cf8215d
dmitriikuriavyi <valerichdmitriy@gmail.com>
2018-07-10 21:52:18 +0300

    sprboottask2

--- src/test/java/ru/otus/ApplicationTests.java
+++ src/test/java/ru/otus/ApplicationTests.java
@@ -2,15 +2,31 @@ package ru.otus;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.otus.dao.ParserCsvDao;
+import ru.otus.model.Question;
+import ru.otus.service.ParserCsvService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
+
+    @MockBean
+    private ParserCsvDao parserCsvDao;
+    @Autowired
+    private ParserCsvService parserCsvService;
 
     @Test
     public void contextLoads() {
+        List<Question> questions = new ArrayList<>();
+
+        questions.a
     }
 
 }
